75f7d2579ac603cf386986b11c5d5dfbeedeebbe
Andrea Talenti <andrea.talenti@ed.ac.uk>
2020-04-27 11:28:43 +0100

    Script to convert tree to PhyloXml now working

--- NewickToXml.py
+++ NewickToXml.py
@@ -3,10 +3,14 @@
 def parser():
     import argparse
     parse = argparse.ArgumentParser()
-    parse.add_argument("-i", "--input", meta = "infile", required = True, dest = "intree", type = str)
-    parse.add_argument("-o", "--output", meta = "outfile", required = True, dest = "outtree", type = str)
-    parse.add_argument("-f", "--infmt", meta = "nexus/newick/phyloxml", required = True, dest = "infmt", type = str)
-    parse.add_argument("-F", "--outfmt", meta = "nexus/newick/phyloxml", required = True, dest = "outfmt", type = str)
+    parse.add_argument("-i", "--input", metavar = 'file', type = str, help = 'Input tree', \
+                            default = None, dest = 'infile', required=True)
+    parse.add_argument("-o", "--output", metavar = 'file', type = str, help = 'Output tree (default = outfile)', \
+                            default = "outtree", dest = 'outfile', required=False)
+    parse.add_argument("-f", "--infmt", metavar = "nexus/newick/phyloxml", type = str, help = 'Input tree format', \
+                            default = None, dest = 'infmt', required=True)
+    parse.add_argument("-F", "--outfmt", metavar = "nexus/newick/phyloxml", type = str, help = 'Output tree format', \
+                            default = "phyloxml", dest = 'outfmt', required=False)
     return parse.parse_args()
 
 
