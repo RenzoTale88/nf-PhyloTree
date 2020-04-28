228bbe7d9533093a1b8d2371a267167643c69747
Andrea Talenti <andrea.talenti@ed.ac.uk>
2020-04-28 09:34:53 +0100

    Consensus tree accept command line file name

--- ConsensusTree.py
+++ ConsensusTree.py
@@ -3,7 +3,8 @@
 def main():
     from Bio import Phylo
     from Bio.Phylo.Consensus import *
-    trees = list(Phylo.parse('Tests/TreeConstruction/trees.tre', 'newick'))
+    import sys
+    trees = list(Phylo.parse(sys.argv[1], 'newick'))
     majority_tree = majority_consensus(trees, 0)
     Phylo.write(majority_tree, 'consensus.xml', 'phyloxml')    
 
